abbc986aac5836fedb4f6870dc1e78cb9ff53e91
ido-netaneli <ido0690@gmail.com>
2018-10-31 17:57:19 +0200

    adding starting project

---
+++ script.js
@@ -0,0 +1,40 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById('gradient');
+var btn = document.querySelector("button");
+
+body.style.background = "linear-gradient(to right, " 
+	+ color1.value
+	+", "
+	+ color2.value
+	+")";
+css.textContent = body.style.background + ";";
+
+function setRandomColor(){
+	color1.value = getRandomColor();
+	color2.value = getRandomColor();
+	setGradient();
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function setGradient(){
+	body.style.background =
+	"linear-gradient(to right, " 
+	+ color1.value
+	+", "
+	+ color2.value
+	+")";
+	css.textContent = body.style.background + ";";
+}
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
+btn.addEventListener("click", setRandomColor);
